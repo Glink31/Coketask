cdf8810803c393218640c62294ce6c199c0e7a3d
Glink31 <56264576+Glink31@users.noreply.github.com>
2021-03-25 18:30:47 +0500

    Add files via upload

--- bottle.py
+++ bottle.py
@@ -10,12 +10,17 @@ class Bottle(metaclass=ABCMeta):
     def close(self,cap):
         if self.cap is None: 
             self.cap = cap
+            print(f"закрыли бутылку {self.brand} крышкой {cap.brand}")
     @abstractmethod
     def fill(self,drink):
         if self.cap is None:
             if self.drink is None:
-                self.drink = drink     
+                self.drink = drink
+                print(f"залили {drink.brand} в бутылку {self.brand}")
+        else:
+            print("нельзя залить напиток, потому что бутылка закрыта")     
     @abstractmethod
     def applylabel(self,label):
         if self.label is None:
             self.label = label
+            print(f"наклеили этикетку {label.brand} на бутылку {self.brand}")
